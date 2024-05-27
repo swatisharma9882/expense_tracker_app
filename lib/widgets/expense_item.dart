6fe852f5f9d16833bd15f69e7f761fea085e4723
Swatishrama9882 <swatishrama9882@gmail.com>
2024-05-27 18:13:11 +0530

    theme setup

--- lib/widgets/expense_item.dart
+++ lib/widgets/expense_item.dart
@@ -12,8 +12,11 @@ class ExpenseItem extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-        child: Column(children: [
-          Text(expense.title),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            expense.title,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
           const SizedBox(height: 4),
           Row(children: [
             Text("\$${expense.amount.toStringAsFixed(2)}"),
